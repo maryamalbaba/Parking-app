6c60991aa41f2fb380fc5b726a2d6488302696f8
unknown <maryam>
2024-06-09 14:42:06 +0300

    core app

---
+++ lib/core/resourses/Images_name.dart
@@ -0,0 +1,7 @@
+class ImagesName{
+  String anyWhere="assets/anywhere.png";
+  String packcar="assets/parkcar.png";
+  String AtanyTime="assets/Atanytime.png";
+
+
+}
---
+++ lib/core/resourses/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color darkgreen=Color(0Xff008955);
